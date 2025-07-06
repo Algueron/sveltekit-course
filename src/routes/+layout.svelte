21d79f6b26c04ae723aea316509ecb56785dd3c7
Julien Papillon <julien.papillon@gmail.com>
2025-07-06 17:53:30 +0200

    feat: Add nested routes and layouts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,15 @@
 	let { children } = $props();
 </script>
 
-<nav class="my-2 bg-amber-600 p2">
-	<ul class="flex gap-2">
-		<li>
-			<a href="/">Home</a>
-			<a href="/about">About</a>
-		</li>
-	</ul>
-</nav>
-{@render children()}
+<div class="border-2 bg-pink-900 p-4">
+	<h1 class="text-3x1">Root Layout</h1>
+	<nav class="my-2 bg-amber-600 p2">
+		<ul class="flex gap-2">
+			<li>
+				<a href="/">Home</a>
+				<a href="/about">About</a>
+			</li>
+		</ul>
+	</nav>
+	{@render children()}
+</div>
