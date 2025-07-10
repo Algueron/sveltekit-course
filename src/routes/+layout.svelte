e561e458fcc67db8a8e4d094f9f0c742f22562bb
Julien Papillon <julien.papillon@gmail.com>
2025-07-10 21:33:10 +0200

    feat: Use page data to customize the title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<title>{page.data.title ? `${page.data.title}|NoteNow` : 'NoteNow'}</title>
+	<meta property="og:title" content="NoteNow" />
+	<meta property="og:description" content="Some description" />
+	<meta property="description" content="Some description" />
+</svelte:head>
 
 {@render children()}
