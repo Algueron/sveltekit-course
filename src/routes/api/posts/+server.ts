1e5f0fa1cd18a47a541622c250c33bfd37ef88dd
Algueron <julien.papillon@gmail.com>
2025-07-11 11:48:14 +0200

    feat: Handle POST and fallbacks

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,4 +1,4 @@
-import { json } from "@sveltejs/kit";
+import { error, json, text } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 
 export const GET: RequestHandler = async({fetch}) => {
@@ -6,3 +6,17 @@ export const GET: RequestHandler = async({fetch}) => {
     const postResJSON = await postsRes.json();
     return json(postResJSON, { status: postsRes.status });
 };
+
+export const POST: RequestHandler = async({request}) => {
+    const post = await request.json();
+    
+    if(!post.title) {
+        error(400, 'Post title is required!');
+    }
+    // Insert post into db
+    return json({ id: crypto.randomUUID(), title: post.title });
+}
+
+export const fallback: RequestHandler = async({request}) => {
+    return text(`${request.method} received!`);
+}
